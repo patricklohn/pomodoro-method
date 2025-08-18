e34fe3b9f53d124528e1cb7866a1a5e60c107c79
patricklohn <patricklohn3@gmail.com>
2025-08-18 07:41:06 -0300

    Configuração hook form

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,17 +1,26 @@
 import {Play} from 'phosphor-react';
 import {HomeContainer, FormContainer, CountdownContainer, Separator, TaskInput, MinutesAmountInput} from './styles';
+import {useForm} from 'react-hook-form';
 import { useState } from 'react';
 
 function Home() {
-    const [task, setTask] = useState('');
+    const { register, handleSubmit, watch } = useForm()
 
-    function resetForm() {
-        setTask('');
+    // const [task, setTask] = useState('');
+    const task = watch('task')
+    const isSubmitDisabled = !task
+
+    function handleCreateNewCycle(data:any){
+        console.log(data)
     }
+
+    // function resetForm() {
+    //     setTask('');
+    // }
 
     return (
         <HomeContainer>
-            <form onSubmit={alert}>
+            <form onSubmit={handleSubmit(handleCreateNewCycle)}>
                 <FormContainer>
                     <label htmlFor="task">Vou trabalhar Em</label>
                     <TaskInput
@@ -19,8 +28,9 @@ function Home() {
                         id="task" 
                         list='task-suggestions' 
                         placeholder='Dê um nome para o seu Projeto' 
-                        value={task} 
-                        onChange={e => setTask(e.target.value)}
+                        // value={task} 
+                        // onChange={e => setTask(e.target.value)}
+                        {...register('task')}
                     />
                     <datalist id='task-suggestions'>
                         <option value="Projeto 1"></option>
@@ -34,6 +44,7 @@ function Home() {
                         step={5} 
                         min={5} 
                         max={60}
+                        {...register('minutesAmount', {valueAsNumber: true})}
                     />
                 <span>minutos.</span>   
                 </FormContainer>
@@ -44,7 +55,7 @@ function Home() {
                     <span>0</span>
                     <span>0</span>
                 </CountdownContainer>
-                <button type="submit" disabled={!task}>
+                <button type="submit" disabled={isSubmitDisabled}>
                     <Play size={24}/>
                     Começar
                 </button>
